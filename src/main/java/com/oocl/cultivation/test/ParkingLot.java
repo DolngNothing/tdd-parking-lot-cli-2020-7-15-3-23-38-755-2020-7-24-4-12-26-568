47b7f1752b96816df14c723c31a8a167e63af5da
DolngNothing <1178363215@qq.com>
2020-07-26 16:42:23 +0800

    [christopher] feature: pass the test should_return_null_ticket_when_fetch_given_over_capacity

--- src/main/java/com/oocl/cultivation/test/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/test/ParkingLot.java
@@ -8,7 +8,18 @@ public class ParkingLot {
 
     private List<CarTicket> carTickets = new ArrayList<CarTicket>();
 
+    private Integer capacity;
+
+    public ParkingLot(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public ParkingLot() {
+        this.capacity=10;
+    }
+
     public CarTicket park(Car car) {
+        if(carTickets.size()>=this.capacity) return null;
         CarTicket carTicket = new CarTicket(car);
         carTickets.add(carTicket);
         return carTicket;
