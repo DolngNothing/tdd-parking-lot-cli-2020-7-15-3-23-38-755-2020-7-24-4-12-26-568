2102543e76601a85cdd490ffb3f11ceb3f5c2931
DolngNothing <1178363215@qq.com>
2020-07-27 20:08:42 +0800

    [christopher] refactor:delete useless variable used

--- src/main/java/com/oocl/cultivation/test/CarTicket.java
+++ src/main/java/com/oocl/cultivation/test/CarTicket.java
@@ -2,16 +2,7 @@ package com.oocl.cultivation.test;
 
 public class CarTicket {
 
-    private Boolean isUsed;
 
     public CarTicket() {
-    }
-
-    public Boolean getUsed() {
-        return isUsed;
-    }
-
-    public void setUsed(Boolean used) {
-        isUsed = used;
     }
 }
